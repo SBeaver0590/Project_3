cfbdc8422da2666590e8d77e35b2618b31894b1a
Simone Beaver <simone.beaver@verizon.com>
2019-07-08 06:04:52 -0500

    paymentSecUpdate

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
    
    
     //Job Role Section   
-    $('#other-title').hide(); //target the other input field.
+    $('#other-title').hide(); //target the other input field and hide it.
     $('#title').on('click', function(event){ // here is where i said if th               cdcdcd 
 
         if ($('#title').val() == 'other') {
@@ -37,7 +37,7 @@ $(document).ready(function() {
         let parentLabelText = $(this)[0].parentElement.innerText; // Get the parent label text of the selected checkbox
         let dollarIndex = parentLabelText.indexOf("$");  // Get the dollar index in the selected checkbox parent label text
         let activityCost = parentLabelText.slice(dollarIndex + 1);  // Get the activity cost from the selected checkbox parent label text
-    
+    });
      
        
     
@@ -56,7 +56,7 @@ $(document).ready(function() {
         let selectedCheckboxDayAndTime = parentLabelText.slice(selectedCheckboxEmDashIndex + 1, selectedCheckboxCommaIndex);
         
         // Loop through all the checkboxes to see if there are any conflicting events if so disable the buttons option
-        //as well as not adding it to the total cost.
+        //as well as not being able to add it to the total cost.
           for (let j = 0; j < $(":checkbox").length; j++) {
           let checkBoxParentLabelText = $(":checkbox")[j].parentElement.innerText;
           let checkBoxEmDashIndex = checkBoxParentLabelText.indexOf("\u2014");
@@ -70,13 +70,28 @@ $(document).ready(function() {
              $(":checkbox")[j].disabled = false;
             }
          }
-        }
+          }
       });
+    //Payment Section
+    $('#payment').val('credit card');
+    $('p:first').hide();
+    $('p:last').hide();
+
+    $('#payment').change(function() {
+    if($('#payment option:selected').val() === 'paypal') {
+        $('#credit-card, #bitcoin, p:last').hide();
+        $('#paypal, p:first').show();
+    } else if ($('#payment option:selected').val() === "bitcoin") {
+        $('#credit-card, #paypal, p:first').hide();
+        $('#bitcoin, p:last').show();
+    } else {
+        $('#credit-card').show();
+        $('#paypal, #bitcoin, p:first, p:last').hide();
+    }
+    //Form Validation Section
+    var emailAddress = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+    var creditCard = /\b\d{4}(| |-)\d{4}\1\d{4}\1\d{4}\b/g;
+    var zipCode = /^\d{5}(?:[-\s]\d{4})?$/;
+    var errorMessage ="";
+    
 });  
-
-
-
-
-
-
-
